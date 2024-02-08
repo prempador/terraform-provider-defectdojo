ab2af5aa71e9043212ef54a1bab6a3f067b8396e
Benjamin Muellner <muellner.benjamin@gmail.com>
2024-02-08 23:02:57 +0100

    ✨ Add Product Types Data Source

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -230,7 +230,9 @@ func (p *DefectdojoProvider) Resources(ctx context.Context) []func() resource.Re
 }
 
 func (p *DefectdojoProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{}
+	return []func() datasource.DataSource{
+		NewProductTypesDataSource,
+	}
 }
 
 func New(version string) func() provider.Provider {
