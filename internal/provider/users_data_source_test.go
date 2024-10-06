8d9655a13a7191260b0b6771859c4535fb34bef7
Benjamin <muellner.benjamin@gmail.com>
2024-10-06 13:31:59 +0200

    ✨ Add Users Data Source (#63)
    * ✨ Add Users Data Source
    
    * ✅ Fix Datasource Tests
    
    * ✅ Remove test cases that require ordering

---
+++ internal/provider/users_data_source_test.go
@@ -0,0 +1,46 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccUsersDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				Config: providerConfig + `
+				resource "defectdojo_user" "test1" {
+					username = "User1"
+					email	 = "email1@email.com"
+					password = "veryHardPassword1234!"
+				}
+
+				resource "defectdojo_user" "test2" {
+					username = "User2"
+					email	 = "email2@email.com"
+					password = "veryHardPassword1234!"
+				}
+                `,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify users were created
+					resource.TestCheckResourceAttr("defectdojo_user.test1", "username", "User1"),
+					resource.TestCheckResourceAttr("defectdojo_user.test2", "username", "User2"),
+				),
+			},
+			{
+				Config: providerConfig + `data "defectdojo_users" "test" {}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify number of users returned
+					resource.TestCheckResourceAttr("data.defectdojo_users.test", "users.#", "3"), // need to check for 3 because the default user is created
+				),
+			},
+		},
+	})
+}
