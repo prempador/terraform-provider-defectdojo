1ae880e6326d06425e2c6d0e20388b30a27c6b02
Benjamin <muellner.benjamin@gmail.com>
2025-01-16 16:12:39 +0100

    :green_heart: Fix Workflows and Reduce Main go.mod (#81)
    * :green_heart: Fix Workflows and Reduce Main go.mod
    
    * :green_heart: Fix Generate Command

--- tools/tools.go
+++ tools/tools.go
@@ -1,11 +1,19 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-//go:build tools
+//go:build generate
 
 package tools
 
 import (
 	// Documentation generation
+	_ "github.com/hashicorp/copywrite"
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
+
+// Format Terraform code for use in documentation.
+// If you do not have Terraform installed, you can remove the formatting command, but it is suggested
+// to ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ../examples/
+// Generate documentation.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name terraform-provider-defectdojo
